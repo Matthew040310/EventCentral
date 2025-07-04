a65d430ff87583eec42686ce2aeb50065bd32059
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-04 13:40:36 +0800

    test : amend file type to address build errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // ...any other config you need
 };
 
-export default nextConfig;
+module.exports = nextConfig;
