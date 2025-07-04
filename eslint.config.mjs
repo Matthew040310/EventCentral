f053ef4c1c491ac5d0f23aa6e25562864a45c2c6
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-04 13:30:18 +0800

    test : config changes for UAT testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // Add your overrides here
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/jsx-key": "off",
+      "prefer-const": "off",
+      "no-var": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
