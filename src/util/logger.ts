1ab046ffaac53df8bd0be9309c50ebeb5623799c
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-04 14:05:53 +0800

    fix : comment out logger function. Not required for UAT

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,18 +1,18 @@
-import pino from 'pino';
-import { Writable } from 'stream';
+// import pino from 'pino';
+// import { Writable } from 'stream';
 
 
-const transport = new Writable({
-  write(chunk:any, enc:any, cb:any) {
-    const msg = JSON.parse(chunk.toString());
-    const level = msg.level;
-    const logFunction = (level <= 30) ? console.log : (level === 40) ? console.warn : console.error;
+// const transport = new Writable({
+//   write(chunk:any, enc:any, cb:any) {
+//     const msg = JSON.parse(chunk.toString());
+//     const level = msg.level;
+//     const logFunction = (level <= 30) ? console.log : (level === 40) ? console.warn : console.error;
     
-    logFunction(chunk.toString());
-    cb();
-  }
-});
+//     logFunction(chunk.toString());
+//     cb();
+//   }
+// });
 
-const logger = pino(transport)
+// const logger = pino(transport)
 
-export default logger
+// export default logger
