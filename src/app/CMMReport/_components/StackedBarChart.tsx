5330c20af2bde1f593b9f872cf7f41119b3f322a
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-18 22:38:04 +0800

    chore (StackedBarChart) : rename dummy constants

--- src/app/CMMReport/_components/StackedBarChart.tsx
+++ src/app/CMMReport/_components/StackedBarChart.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { Box, Typography } from '@mui/material';
+import { Event_Legend } from '@/styles/theme';
 
 const xAxisLabel = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", " Oct", "Nov", "Dec"];
 
-const NEW_2025 = {
+const HighImpact = {
     data: [3, 1, 4, 2, 1, 3, 2, 4, 5, 1, 2, 3],
-    label: "New",
-    stack: '2025',
-    color: 'Orange'
+    stack: "total",
+    label: "High Impact",
+    color: Event_Legend["High Impact"]
 };
-const CHANGE_2025 = {
+const Change_New = {
     data: [2, 1, 4, 2, 1, 3, 2, 4, 5, 1, 2, 3],
-    label: "Existing with Change",
-    stack: '2025',
-    color: 'Blue'
+    stack: "total",
+    label: "Change / New",
+    color: Event_Legend["New/Changes"]
 };
-const EXISTING_2025 = {
+const Existing = {
     data: [3, 1, 4, 2, 1, 3, 2, 4, 5, 1, 2, 3],
+    stack: "total",
     label: "Existing",
-    stack: '2025',
-    color: 'Grey'
+    color: Event_Legend["Existing"]
 };
 
 const StackedBarChart: React.FC = () => {
@@ -43,9 +44,9 @@ const StackedBarChart: React.FC = () => {
                     grid={{ horizontal: true }}
                     series={
                         [
-                            { ...EXISTING_2025 },
-                            { ...CHANGE_2025 },
-                            { ...NEW_2025 },
+                            { ...HighImpact },
+                            { ...Change_New },
+                            { ...Existing },
                         ]}
                 />
             </Box >
