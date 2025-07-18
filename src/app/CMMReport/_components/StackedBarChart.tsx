9ed1ca2024a9ee6423534c50f7db3e6939d5599c
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-18 23:12:40 +0800

    feat (StackedBarChart) : integrate new function for stackedBarChart data

--- src/app/CMMReport/_components/StackedBarChart.tsx
+++ src/app/CMMReport/_components/StackedBarChart.tsx
@@ -1,33 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import dayjs from 'dayjs';
 
 import { BarChart } from '@mui/x-charts/BarChart';
 import { Box, Typography } from '@mui/material';
-import { Event_Legend } from '@/styles/theme';
-import determineCategory from '@/util/determineCategory';
+import categoriseMonthlyEvents from '../_functions/categoriseMonthlyEvents';
 
 import useDashboardEventReports from '@/hooks/useDashboardEventReports';
 
 const xAxisLabel = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", " Oct", "Nov", "Dec"];
-
-const HighImpact = {
-    data: [3, 1, 4, 2, 1, 3, 2, 4, 5, 1, 2, 3],
-    stack: "total",
-    label: "High Impact",
-    color: Event_Legend["High Impact"]
-};
-const Change_New = {
-    data: [2, 1, 4, 2, 1, 3, 2, 4, 5, 1, 2, 3],
-    stack: "total",
-    label: "Change / New",
-    color: Event_Legend["New/Changes"]
-};
-const Existing = {
-    data: [3, 1, 4, 2, 1, 3, 2, 4, 5, 1, 2, 3],
-    stack: "total",
-    label: "Existing",
-    color: Event_Legend["Existing"]
-};
 
 interface StackedBarChartProps {
     datumDate: Date;
@@ -45,6 +25,10 @@ const StackedBarChart: React.FC<StackedBarChartProps> = ({
             const endOfYear = dayjs(datumDate).endOf('year').toDate();
             return { startDate: { gte: startOfYear, lte: endOfYear } }
         }, [year]);
+
+    const { HighImpact, Change_New, Existing } = useMemo(() => {
+        return categoriseMonthlyEvents(submittedEventReports);
+    }, [submittedEventReports]);
 
     return (
         <>
