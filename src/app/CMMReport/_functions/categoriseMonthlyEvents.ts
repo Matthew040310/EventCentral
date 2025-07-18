5648c28886f5b42e8922fb7030f89f8e53ca2f7d
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-18 23:12:02 +0800

    feat (StackBarChart) : create function to group submittedEvents by month and category

---
+++ src/app/CMMReport/_functions/categoriseMonthlyEvents.ts
@@ -0,0 +1,60 @@
+import dayjs from 'dayjs';
+import FullEventReport from '@/types/IFullEventReport';
+import determineCategory from '@/util/determineCategory';
+import { Event_Legend } from '@/styles/theme';
+
+interface CategoryData {
+    data: number[];
+    stack: "total";
+    label: string;
+    color: string;
+}
+
+interface CategorizedResult {
+    HighImpact: CategoryData;
+    Change_New: CategoryData;
+    Existing: CategoryData;
+}
+
+export default function categorizeMonthlyEvents(submittedEventReports: FullEventReport[]): CategorizedResult {
+    // Prepare empty arrays for 12 months
+    const highImpactCounts = Array(12).fill(0);
+    const changeNewCounts = Array(12).fill(0);
+    const existingCounts = Array(12).fill(0);
+
+    submittedEventReports.forEach((event) => {
+        const month = dayjs(event.startDate).month();
+        const category = determineCategory(event).label;
+
+        if (category === "High Impact") {
+            highImpactCounts[month]++;
+        }
+        else if (category === "New/Changes") {
+            changeNewCounts[month]++;
+        }
+        else if (category === "Existing") {
+            existingCounts[month]++;
+        }
+    })
+
+    return {
+        HighImpact: {
+            data: highImpactCounts,
+            stack: "total",
+            label: "High Impact",
+            color: Event_Legend["High Impact"]
+        },
+        Change_New: {
+            data: changeNewCounts,
+            stack: "total",
+            label: "Change / New",
+            color: Event_Legend["New/Changes"]
+        },
+        Existing: {
+            data: existingCounts,
+            stack: "total",
+            label: "Existing",
+            color: Event_Legend["Existing"]
+        }
+    }
+};
