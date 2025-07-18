108cd6abba4e7afec6ed31c844cd152dc4114ca2
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-18 19:07:53 +0800

    fix (getDashboardData) : amend code to return Promise directly

--- src/util/getDashboardData.ts
+++ src/util/getDashboardData.ts
@@ -1,29 +1,27 @@
 import getFullEventReports from '@/util/Prisma-API-handlers/getFullEventReports';
 import { FullEventReportWithFilters } from '@/types/IFullEventReportParams';
 
-export default function getDashboardData(prismaFilters: Partial<FullEventReportWithFilters>) {
-    return async () => {
-        try {
-            const allSubmissions = await getFullEventReports({
-                state: 'Submitted',
-                sortby: [{ type: 'desc' }, { estimatedCohortSize: 'desc' }],
-                filters: prismaFilters,
-            });
+export default async function getDashboardData(prismaFilters: Partial<FullEventReportWithFilters>) {
+    try {
+        const allSubmissions = await getFullEventReports({
+            state: 'Submitted',
+            sortby: [{ type: 'desc' }, { estimatedCohortSize: 'desc' }],
+            filters: prismaFilters,
+        });
 
-            const allDrafts = await getFullEventReports({
-                state: 'Draft',
-                sortby: [{ type: 'desc' }, { estimatedCohortSize: 'desc' }],
-                filters: prismaFilters,
-            });
+        const allDrafts = await getFullEventReports({
+            state: 'Draft',
+            sortby: [{ type: 'desc' }, { estimatedCohortSize: 'desc' }],
+            filters: prismaFilters,
+        });
 
-            return {
-                submitted: allSubmissions,
-                draft: allDrafts
-            };
-        }
-        catch (error) {
-            console.error('Error fetching event reports:', error);
-            return { submitted: [], draft: [] };
-        }
+        return {
+            submitted: allSubmissions,
+            draft: allDrafts
+        };
+    }
+    catch (error) {
+        console.error('Error fetching event reports:', error);
+        return { submitted: [], draft: [] };
     }
 }
