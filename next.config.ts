6bdd165d14df817f79c9028a0f3a7762be869b62
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-22 11:30:04 +0800

    feat (config) : amend next.js config to reference .env for APP_BASE_PATH

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const APP_BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: '/event-central',
-  assetPrefix: '/event-central',
+  basePath: APP_BASE_PATH,
+  assetPrefix: APP_BASE_PATH,
 };
 
 export default nextConfig;
