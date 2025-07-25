ed62dfd776e11c89b554f5895e18aff6fac0349e
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-25 17:52:11 +0800

    feat (EventDialogTitle) : add Event Date into UI render

--- src/components/_EventDetailsDialog/TitleContent.tsx
+++ src/components/_EventDetailsDialog/TitleContent.tsx
@@ -1,6 +1,7 @@
 import { Stack, Typography, Chip } from '@mui/material';
 import { EventAvailable } from '@mui/icons-material';
 import FullEventReport from '@/types/IFullEventReport';
+import dateFormatter from '@/util/dateFormatter';
 
 interface TitleContentProps {
     eventDetails: Partial<FullEventReport>;
@@ -11,18 +12,28 @@ const TitleContent: React.FC<TitleContentProps> = ({
     eventDetails,
     eventColor,
 }) => {
-    const { title, embargoed } = eventDetails;
+    const { title, eventDate, embargoed } = eventDetails;
 
     return (
-        <Stack direction="row" alignItems="center" spacing={1}>
-            <EventAvailable sx={{ color: eventColor }} />
-            <Typography variant="h6" component="span" sx={{ flexGrow: 1 }} color={eventColor}>
-                {title}
-            </Typography>
-            {embargoed === "Yes" &&
-                <Chip label="Embargoed" size="small" sx={{ color: eventColor }} />
-            }
-        </Stack>
+        <>
+            <Stack direction="row" alignItems="center" spacing={1}>
+                <EventAvailable sx={{ color: eventColor }} />
+                <Typography variant="h6" component="span" sx={{ flexGrow: 1 }} color={eventColor}>
+                    {title}
+                </Typography>
+                {embargoed === "Yes" &&
+                    <Chip label="Embargoed" size="small" color="error" />
+                }
+            </Stack>
+            <Stack direction="row" alignItems="center" spacing={1} mt={0.2}>
+                <Typography variant="subtitle1" component="span" fontWeight="bold">
+                    Event Date:
+                </Typography>
+                <Typography variant="subtitle1" component="span">
+                    {dateFormatter(eventDate as Date)}
+                </Typography>
+            </Stack>
+        </>
     )
 }
 
