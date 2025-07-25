57860bc501c682fe5a83cf0bfc6cdd00d3df40ea
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-25 18:50:09 +0800

    feat (EventStatistics) : modify code to include new category render logic

--- src/util/summariseEventStats.ts
+++ src/util/summariseEventStats.ts
@@ -2,21 +2,24 @@ import FullEventReport from '@/types/IFullEventReport';
 import determineCategory from "@/util/determineCategory";
 
 export default function eventStatistics(eventDetailsArray: Partial<FullEventReport>[]) {
-    const highImpact = new Set<string>();
-    const newChanges = new Set<string>();
-    const existing = new Set<string>();
+    const HighImpact = new Set<string>();
+    const Change_New = new Set<string>();
+    const Existing_MoreThan100k = new Set<string>();
+    const Existing_LessThan100k = new Set<string>();
 
     for (const evt of eventDetailsArray) {
         const cat = determineCategory(evt).label;
-        if (cat === "High Impact") highImpact.add(evt.parentid as string);
-        else if (cat === "New/Changes") newChanges.add(evt.parentid as string);
-        else if (cat === "Existing") existing.add(evt.parentid as string);
+        if (cat === "High Impact") HighImpact.add(evt.parentid as string);
+        else if (cat === "New/Changes") Change_New.add(evt.parentid as string);
+        else if (cat === "Existing >=100k") Existing_MoreThan100k.add(evt.parentid as string);
+        else if (cat === "Existing <100k") Existing_LessThan100k.add(evt.parentid as string);
     }
 
     return {
-        total: (highImpact.size + newChanges.size + existing.size),
-        highImpact: highImpact.size,
-        newChanges: newChanges.size,
-        existing: existing.size,
+        total: (HighImpact.size + Change_New.size + Existing_MoreThan100k.size + Existing_LessThan100k.size),
+        HighImpact: HighImpact.size,
+        Change_New: Change_New.size,
+        Existing_MoreThan100k: Existing_MoreThan100k.size,
+        Existing_LessThan100k: Existing_LessThan100k.size,
     }
 }
