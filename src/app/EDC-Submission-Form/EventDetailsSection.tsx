3cc263738baa23647852cfbf9d7fe4fb32573415
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-25 17:54:36 +0800

    refactor (EventDetailsSection) : condense if else statements

--- src/app/EDC-Submission-Form/EventDetailsSection.tsx
+++ src/app/EDC-Submission-Form/EventDetailsSection.tsx
@@ -20,12 +20,8 @@ const EventDetailsSection: React.FC<EventDetailsProps> = ({
     // Initialize knowStartDate based on async inputFields.startDate change
     useEffect(() => {
         // Check both startDate and estimatedStartDate
-        if (inputFields?.startDate) {
-            setKnowStartDate("Yes");
-        }
-        else if (inputFields?.estimatedStartDate) {
-            setKnowStartDate("No");
-        }
+        if (inputFields?.startDate) setKnowStartDate("Yes");
+        else if (inputFields?.estimatedStartDate) setKnowStartDate("No");
     }, [inputFields?.startDate, inputFields?.estimatedStartDate]);
 
     useEffect(() => {
