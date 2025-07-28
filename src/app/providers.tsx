8cf6cba17aaac5c67448e932667a4d5498d1aa9c
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-28 17:45:48 +0800

    chore (dep) : import NextAuth for useSession hook

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-// import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@mui/material/styles";
 import { SnackbarProvider } from "notistack";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,14 +9,14 @@ import theme from "@/styles/theme";
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <AppRouterCacheProvider>
-            {/* <SessionProvider> */}
-            <ThemeProvider theme={theme}>
-                <SnackbarProvider maxSnack={3}>
-                    <CssBaseline />
-                    {children}
-                </SnackbarProvider>
-            </ThemeProvider>
-            {/* </SessionProvider> */}
+            <SessionProvider>
+                <ThemeProvider theme={theme}>
+                    <SnackbarProvider maxSnack={3}>
+                        <CssBaseline />
+                        {children}
+                    </SnackbarProvider>
+                </ThemeProvider>
+            </SessionProvider>
         </AppRouterCacheProvider>
     );
 }
