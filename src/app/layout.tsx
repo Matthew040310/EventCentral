047f4e31e2d46c9f0d1129bec2f65de6be25ec80
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-07-28 16:06:10 +0800

    refactor (app) : organise wraooer components to provider.tsx. Layout.tsx used solely for UI render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,20 @@
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { Analytics } from "@vercel/analytics/next"
-import Providers from "./providers";
 import "./globals.css";
+import Providers from "./providers";
 import CustomNavBar from "@/components/CustomNavBar";
+import { Analytics } from "@vercel/analytics/next";
 import CustomFooter from "@/components/CustomFooter";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <AppRouterCacheProvider>
-          <Providers>
-            <CustomNavBar />
-            {children}
-            <Analytics />
-            <CustomFooter />
-          </Providers>
-        </AppRouterCacheProvider>
-      </body>
-    </html >
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Providers>
+                    <CustomNavBar />
+                    {children}
+                    <Analytics />
+                    <CustomFooter />
+                </Providers>
+            </body>
+        </html>
+    );
 }
