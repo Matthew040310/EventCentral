33232d48fdf771af719b2f1d5b977ec58567a27c
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-04 17:01:27 +0800

    feat (CustomNavBar) : add conditional logic for manageUserButton rendering

--- src/components/CustomNavBar.tsx
+++ src/components/CustomNavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { useSession } from "next-auth/react";
 import { AppBar, Box, Grid, IconButton, Toolbar, Menu, MenuItem, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { AccountCircle, AddCircleOutline, Assessment, CalendarMonth, Home, Group, Logout, ManageAccounts, Search, Info } from '@mui/icons-material';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -7,6 +8,8 @@ import NavBarButton from "./NavBarButton";
 
 const CustomNavBar: React.FC = () => {
   const theme = useTheme();
+  const { data: session } = useSession();
+  console.log("Session Data:", session);
 
   // Collapsable Menu State
   const isSmallScreen = useMediaQuery(theme.breakpoints.down(750));       // true when screen width <= 750px
@@ -17,15 +20,15 @@ const CustomNavBar: React.FC = () => {
   // const InfoButton = <NavBarButton href="#" icon={<Info />} title="Wiki" /> // Future Development
   const SubmitNewEventButton = <NavBarButton href="/EDC-Submission-Form" icon={<AddCircleOutline />} title="Submit New Event" />
   const SearchButton = <NavBarButton href="/SearchEvents" icon={<Search />} title="Search Events" />
-  const ManageUsersButton = <NavBarButton href="/ManageUsers" icon={<ManageAccounts />} title="Manage Users" />
+  const ManageUsersButton = session ? <NavBarButton href="/ManageUsers" icon={<ManageAccounts />} title="Manage Users" /> : null;
   const LogoutButton = <NavBarButton href="/" icon={<Logout />} title="Exit Event Central" iconAsContent={true} />
 
-  // const loggedInUser = (
-  //   session?.user &&
-  //   <Typography fontSize={16} variant="subtitle2" sx={{ display: 'flex', alignItems: 'center' }}>
-  //     <AccountCircle sx={{ mr: 1 }} /><span className="span-bold-underline">{session?.user?.name}</span>
-  //   </Typography>
-  // );
+  const loggedInUser = (
+    session?.user &&
+    <Typography fontSize={16} variant="subtitle2" sx={{ display: 'flex', alignItems: 'center' }}>
+      <AccountCircle sx={{ mr: 1 }} /><span className="span-bold-underline">{session?.user?.name}</span>
+    </Typography>
+  );
 
   return (
     <AppBar position="static">
