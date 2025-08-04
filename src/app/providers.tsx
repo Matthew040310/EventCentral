961a3b41008ceeb6a665e0fbe64efeb298dd5dc8
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-04 17:00:38 +0800

    feat (SessionProvider) : Wrap app content with SessionProvider to provide session context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,17 +3,20 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import { SnackbarProvider } from "notistack";
 import CssBaseline from "@mui/material/CssBaseline";
+import { SessionProvider } from "next-auth/react";
 import theme from "@/styles/theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <AppRouterCacheProvider>
-            <ThemeProvider theme={theme}>
-                <SnackbarProvider maxSnack={3}>
-                    <CssBaseline />
-                    {children}
-                </SnackbarProvider>
-            </ThemeProvider>
+            <SessionProvider>
+                <ThemeProvider theme={theme}>
+                    <SnackbarProvider maxSnack={3}>
+                        <CssBaseline />
+                        {children}
+                    </SnackbarProvider>
+                </ThemeProvider>
+            </SessionProvider>
         </AppRouterCacheProvider>
     );
 }
