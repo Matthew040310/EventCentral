d4cc356239b46d5a9d82d7d4e0df9fab8aa36c9a
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-05 19:28:35 +0800

    style (SignIn) : added text for SignIn page UI

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { useEffect } from "react";
+import { Typography } from "@mui/material";
 
 export default function SignIn() {
     useEffect(() => {
         signIn("google", { callbackUrl: "/eventcentral" });
     }, []);
-    return null;
+
+    return (
+        <Typography variant="h2" textAlign="center" mt={5}>
+            Signing In...
+        </Typography>
+    );
 }
