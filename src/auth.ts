d7a20cd7734d17ed32fb986bf1b6100eaad54485
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-05 18:38:14 +0800

    doc (auth.ts) : add documentation for Auth.js work around

--- src/auth.ts
+++ src/auth.ts
@@ -54,3 +54,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     // debug: process.env.NODE_ENV === 'development',
 })
+
+// --- CAA 5 August 2025 ---
+// NextAuth.js (as of v5 and current Next.js) doesn’t officially support basePath (serving your app at a subdirectory, e.g. /eventcentral)
+// NextAuth and most OAuth flows expect routes like /api/auth/callback/... at the root, and do not resolve paths correctly when your app is configured to live under a subdirectory
+//
+// Key Problems:
+// - Session and callback endpoints break,
+// - Redirects misalign,
+// - “Bad Request” or “UnknownAction: Cannot parse action” errors occur.
+//
+// Workaround:
+// - Modify auth.ts to rewrite request, so that it contains proper hostname and basepath.
+// Reference Link: https://github.com/nextauthjs/next-auth/discussions/12160
