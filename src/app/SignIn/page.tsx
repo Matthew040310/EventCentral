8288185f79f15879f1563209525c0eff9483c27b
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-05 18:31:38 +0800

    feat (SignIn) : automatically redirect to Google Sign In page from SignIn/page.tsx

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -1,22 +1,10 @@
-import { auth, signIn } from "@/auth"
-import { Box, Button } from "@mui/material";
+"use client";
+import { signIn } from "next-auth/react";
+import { useEffect } from "react";
 
-export default async function SignIn() {
-    const session = await auth();
-
-    return (
-        <Box mt={5} component={"div"} display="flex" flexDirection="column" alignItems="center">
-            <form
-                action={async () => {
-                    "use server"
-                    await signIn("google", { redirectTo: "/eventcentral", })
-                }}
-            >
-                <Button type="submit" variant="outlined" color="primary" size="large">
-                    Sign in with Google
-                </Button>
-            </form>
-        </Box>
-
-    )
+export default function SignIn() {
+    useEffect(() => {
+        signIn("google", { callbackUrl: "/eventcentral" });
+    }, []);
+    return null;
 }
