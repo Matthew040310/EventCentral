92b494db99fc38737b65e3cb7a41944d209156bd
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-07 14:53:25 +0800

    feat (Onboarding) : create Onboarding page. To input new user's dept / group / cluster

---
+++ src/app/SignIn/Onboarding/page.tsx
@@ -0,0 +1,127 @@
+// SignIn/Onboarding/page.tsx
+"use client";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { Autocomplete, Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
+
+import { ALL_DEPARTMENTS, ALL_GROUPS, ALL_CLUSTERS, Department_Group_Cluster_Map } from "@/constants/EventCentralConstants";
+
+interface UserDetails {
+    email: string;
+    name: string;
+    department: string;
+    group: string;
+    cluster: string;
+}
+
+const Onboarding = () => {
+    const session = useSession();
+    const [userDetails, setUserDetails] = useState<UserDetails>({
+        email: session.data?.user?.email || "Temporary User",
+        name: session.data?.user?.name || "Temporary Name",
+        department: "",
+        group: "",
+        cluster: "",
+    });
+
+    const handleFieldChange = (fieldName: keyof UserDetails) => (newValue: string | null) => {
+        if (fieldName === "department") {
+            const department = newValue || "";
+            // Get mapped group and cluster for new department or fallback empty
+            const mapping = Department_Group_Cluster_Map[department] || { group: "", cluster: "" };
+
+            setUserDetails((prev) => ({
+                ...prev,
+                department,
+                group: mapping.group,
+                cluster: mapping.cluster,
+            }));
+        } else {
+            setUserDetails((prev) => ({
+                ...prev,
+                [fieldName]: newValue || "",
+            }));
+        }
+    };
+
+    return (
+        <Container maxWidth="md" sx={{ mt: 5 }}>
+            <Typography variant="h2" textAlign="center" gutterBottom>
+                Onboarding
+            </Typography>
+
+            <Typography variant="h6" gutterBottom>
+                Hi there, looks like you are new here!
+            </Typography>
+
+            <Typography variant="subtitle1" mb={5}>
+                To complete your onboarding, please select your <b>department</b> in the following dropdown.<br />
+                Groups and Cluster will be automatically assigned based on your department, but you may edit them as required.
+            </Typography>
+
+            <TextField sx={{ mb: 2 }}
+                fullWidth
+                margin="dense"
+                required
+                disabled
+                label="Email"
+                value={userDetails.email || "Temporary User"}
+            />
+
+            <TextField sx={{ mb: 2 }}
+                fullWidth
+                margin="dense"
+                required
+                disabled
+                label="Name"
+                value={userDetails.name || "Temporary Name"}
+            />
+
+            <Grid container sx={{ mb: 2 }} spacing={2}>
+                <Grid size={{ xs: 12, sm: 4 }}>
+                    <Autocomplete
+                        sx={{ bgcolor: "white" }}
+                        options={ALL_DEPARTMENTS}
+                        value={userDetails.department}
+                        onChange={(event, newValue) => handleFieldChange("department")(newValue)}
+                        renderInput={(params) => (
+                            <TextField {...params} label={"Department"} required={true} />
+                        )}
+                    />
+                </Grid>
+
+                <Grid size={{ xs: 12, sm: 4 }}>
+                    <Autocomplete
+                        sx={{ bgcolor: "white" }}
+                        options={ALL_GROUPS}
+                        value={userDetails.group}
+                        onChange={(event, newValue) => handleFieldChange("group")(newValue)}
+                        renderInput={(params) => (
+                            <TextField {...params} label={"Group"} required={true} />
+                        )}
+                    />
+                </Grid>
+
+                <Grid size={{ xs: 12, sm: 4 }}>
+                    <Autocomplete
+                        sx={{ bgcolor: "white" }}
+                        options={ALL_CLUSTERS}
+                        value={userDetails.cluster}
+                        onChange={(event, newValue) => handleFieldChange("cluster")(newValue)}
+                        renderInput={(params) => (
+                            <TextField {...params} label={"Cluster"} required={true} />
+                        )}
+                    />
+                </Grid>
+            </Grid>
+
+            <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+                <Button variant="contained" color="primary" size="large">
+                    Complete
+                </Button>
+            </Box>
+        </Container>
+    )
+}
+
+export default Onboarding
