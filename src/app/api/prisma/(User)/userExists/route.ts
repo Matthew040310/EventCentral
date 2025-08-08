3a978e572b71236d324a94241a49bc283b49c79c
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-08 10:57:58 +0800

    fix (API | userExists) : add response wrapper for return value. Standardise output format across API endpoints

--- src/app/api/prisma/(User)/userExists/route.ts
+++ src/app/api/prisma/(User)/userExists/route.ts
@@ -3,18 +3,19 @@ import prisma from '@/app/api/prisma/dbClient';
 import primsaErrorHandler from '@/util/Prisma-API-handlers/prismaErrorHandler';
 
 export async function POST(request: Request) {
-    const body = await request.json();
 
     try {
+        const body = await request.json();
         const authorizedUser = await prisma.authorizedUsers.findUnique({
             where: { email: body.email },
         });
 
         return Response.json(
-            { exists: Boolean(authorizedUser) },
+            { response: { exists: Boolean(authorizedUser) } },
             { status: 200 }
         );
     }
+
     catch (error: any) {
         return Response.json(
             { error: primsaErrorHandler("Failed to retrieve Authorized User", error) },
