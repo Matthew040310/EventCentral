3ddcf9a8205c91ee93c4c7f8e9e29213dff27abe
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-08 11:02:25 +0800

    feat (util) : create wrapper function to trigger getAuthorizedUser API endpoint

---
+++ src/util/Prisma-API-handlers/User/getAuthorizedUser.ts
@@ -0,0 +1,25 @@
+const APP_BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+export default async function getAuthorizedUser(email: string) {
+    const targetLink = `${APP_BASE_PATH}/api/prisma/getAuthorizedUser`;
+
+    try {
+        const response = await fetch(targetLink, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email })
+        })
+
+        if (!response.ok) {
+            const errorMessage = await response.json()
+            throw new Error(
+                `HTTP error! Status: ${response.status}.\n${errorMessage.error}`);
+        }
+
+        const result = await response.json();
+        return result.response;
+
+    } catch (error) {
+        throw new Error(`Failed to fetch event report: ${(error as Error).message}`);
+    }
+}
