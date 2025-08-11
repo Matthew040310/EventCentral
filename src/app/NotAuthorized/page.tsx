2a040d6b1d6797389e5140501fda2366cf0a2c4c
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-11 11:39:55 +0800

    feat (page) : create NotAuthorized page

---
+++ src/app/NotAuthorized/page.tsx
@@ -0,0 +1,18 @@
+import { Container, Typography } from "@mui/material";
+
+const NotAuthorized = () => {
+
+    return (
+        <Container maxWidth="sm" sx={{ textAlign: "center", mt: 8 }}>
+            <Typography variant="h3" color="error" gutterBottom>
+                Not Authorized
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 4 }}>
+                You do not have permission to access this page.<br />
+                Please contact your administrator if you believe this is an error.
+            </Typography>
+        </Container>
+    );
+}
+
+export default NotAuthorized;
