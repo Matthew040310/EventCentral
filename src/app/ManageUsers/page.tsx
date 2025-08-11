615307ee18f15a05f29a4b06c5700e35cda5a63d
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-11 11:44:06 +0800

    chore (ManageUser) : structure page.tsx as server component. Verifies session prop before loading client side component

--- src/app/ManageUsers/page.tsx
+++ src/app/ManageUsers/page.tsx
@@ -1,7 +1,22 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
+import ManageUsers_Client from "./ManageUsers.client";
 
-const ManageUsers = () => {
+const ManageUsers = async () => {
+    const session = await auth();
+
+    // TO CHANGE LOGIC TO CHECK USER ROLE
+    if (session?.user?.email === "matthew160619@gmail.com") {
+        redirect("/NotAuthorized")
+    }
+
     return (
-        <div>Manage Users Page in Development</div>
+        <div>
+            <SessionProvider>
+                <ManageUsers_Client />
+            </SessionProvider>
+        </div>
     )
 }
 
