0ea5a804da8c1ffd3769a8836dfcd9c4447c8399
Matthew Lim <matthewlim.2023@smu.edu.sg>
2025-08-14 18:07:06 +0800

    feat (util) : create canDeleteEvent and canEditEvent function

--- src/util/Prisma-API-handlers/User/AccessRights.ts
+++ src/util/Prisma-API-handlers/User/AccessRights.ts
@@ -1,34 +1,55 @@
 import { Session } from "next-auth";
 import FullEventReport from "@/types/IFullEventReport";
 
-function canManageUsers(session: Session): boolean {
+function canManageUsers() {
     // If session.role === "Admin" then return true
     // Else return false
-    return Boolean(null);
+
+    // NOTE: Implemented Directly within ManageUsers/page.tsx using useRequiredRole hook
+    return;
 }
 
-function enableEventTableDeleteAll(session: Session): boolean {
+function enableEventTableDeleteAll() {
     // If session.role === "Admin" then return true
     // Else return false
-    return Boolean(null);
+
+    // NOTE: Implemented Directly within EventTable.tsx using session?.user?.role
+    return;
 }
 
-function canDeleteEvent(session: Session, event: FullEventReport): boolean {
+function canDeleteEvent(session: Session | null, eventDetails: FullEventReport): boolean {
     // If session.role === "Admin" then return true for any event
     // Elseif session.role === "GD" && session.group === event.group then return true
     // Elseif session.role === "HOD" || session.role === "EDC-Rep"
     //      && If session.department === event.department then return true
     // Elseif session.email === event.lastUpdatedBy then return true
     // Else return false
-    return Boolean(null);
+
+    const { role, group, department, email } = session?.user || {};
+
+    if (!session) return false;
+    if (role === "Admin") return true;
+    if (role === "GD" && group === eventDetails.group) return true;
+    if ((role === "HOD" || role === "EDC-Rep") && department.includes(eventDetails.department)) return true;
+    // if (email === eventDetails.lastUpdatedBy) return true;
+
+    return false;
 }
 
-function canEditEvent(session: Session, event: FullEventReport): boolean {
+function canEditEvent(session: Session | null, eventDetails: FullEventReport): boolean {
     // If session.role === "Admin" then return true for any event
     // Elseif session.role === "GD" && session.group === event.group then return true
-    // Elseif session.role === "HOD" || session.role === "EDC-Rep"
-    //      && If session.department === event.department then return true
     // Elseif session.department === event.department then return true
     // Else return false
-    return Boolean(null);
+
+    if (!session) return false;
+
+    const { role, group, department } = session?.user;
+    if (role === "Admin") return true;
+    if (role === "GD" && group === eventDetails.group) return true;
+    if (department.includes(eventDetails.department)) return true;
+
+    return false;
 }
+
+export { canDeleteEvent, canEditEvent }
